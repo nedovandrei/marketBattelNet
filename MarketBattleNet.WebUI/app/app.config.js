690d996d84be098005d9e520dd3a612b74ffbede
Denis Balan <dee.snake@gmail.com>
2016-08-14 22:24:55 +0300

    Added AngularJs to the project.

---
+++ MarketBattleNet.WebUI/app/app.config.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app")
+        .config(["$urlRouterProvider", "$stateProvider", function ($urlRouterProvider, $stateProvider) {
+            $urlRouterProvider.otherwise("/");
+            $stateProvider
+                .state("Home", {
+                    url: "/",
+                    templateUrl: "app/home/home.html",
+                    controller: "HomeController"
+                });
+        }]);
+}());
