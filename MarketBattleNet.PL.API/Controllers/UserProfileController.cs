0c3686c7d27572a3cff6d67669ea1b7ad62cc114
Denis Balan <dbalan@aculocity.com>
2016-09-11 12:38:57 +0300

    Added gitignore and UserProfileController

---
+++ MarketBattleNet.PL.API/Controllers/UserProfileController.cs
@@ -0,0 +1,90 @@
+﻿using MarketBattleNet.BLL.ServiceInterface;
+using MarketBattleNet.BLL.ServiceInterface.DTO;
+using MarketBattleNet.PL.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MarketBattleNet.PL.API.Controllers
+{
+    public class UserProfileController : ApiController
+    {
+        private readonly IUserProfileService _userProfileService;
+
+        public UserProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            var data = _userProfileService.GetAll();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpGet]
+        public HttpResponseMessage FindById(int id)
+        {
+            if(id == 0)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "UserProfileController > FindById(); : Id came in null");
+            }
+
+            var data = _userProfileService.FindById(id);
+            if(data == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"UserProfileController > FindById(); : Nothing found by the id: {id}");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Add(UserProfileViewModel model)
+        {
+            if(model == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "UserProfileController > Add(); : Model Came in null");
+            }
+            var modelDTO = new UserProfileDTO()
+            {
+                FullName = model.FullName,
+                Address = model.Address,
+                PhoneNumber = model.PhoneNumber
+            };
+            _userProfileService.Add(modelDTO);
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Update(UserProfileViewModel model)
+        {
+            if(model == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "UserProfileController > Update(); : Model Came in null");
+            }
+            var modelDTO = new UserProfileDTO()
+            {
+                Id = model.Id,
+                FullName = model.FullName,
+                Address = model.Address,
+                PhoneNumber = model.PhoneNumber
+            };
+            _userProfileService.Update(modelDTO);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            if(id == 0)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "UserProfileController > Delete(); : ID came in null");
+            }
+            _userProfileService.Delete(id);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ MarketBattleNet.PL.API/Models/UserProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketBattleNet.PL.API.Models
+{
+    public class UserProfileViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
